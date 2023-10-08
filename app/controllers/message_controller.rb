c79692d855e81ddbed0f415d549b098a1fa2d5b5
yosaddis <email.to.yoseph@gmail.com>
2023-10-08 05:52:05 +0300

    Fix linter issues

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,7 +1,6 @@
 class MessageController < ApplicationController
-    def index
-      @message = Message.order('RANDOM()').first
-      render json: { greeting: @message.greeting }
-    end
+  def index
+    @message = Message.order('RANDOM()').first
+    render json: { greeting: @message.greeting }
   end
-  
+end
